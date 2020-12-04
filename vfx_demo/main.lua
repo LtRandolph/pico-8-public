5bbad13827827ed3c8a69987f76e709cac6d20a7
Bill Clark <ltrandolphgames@gmail.com>
2020-12-04 13:00:23 -0800

    Add mouse cursor

--- vfx_demo/main.lua
+++ vfx_demo/main.lua
@@ -5,6 +5,9 @@ function _init()
   -- persist palette changes
   -- into editor
   poke(0x5f2e, 1)
+
+  -- enable mouse
+  poke(0x5f2d, 1)
 end
 
 function _update60()
@@ -24,4 +27,19 @@ function _draw()
   -- draw page last so title
   -- will overwrite particles
   draw_pages()
+  
+  mouse_x = stat(32)
+  mouse_y = stat(33)
+  if mouse_x > 1 and
+    mouse_x < 127 and
+    mouse_y > 1 and
+    mouse_y < 127 then
+    line(mouse_x-2, mouse_y,
+      mouse_x+2, mouse_y,
+      palette.white)
+    line(mouse_x, mouse_y-2,
+      mouse_x, mouse_y+2,
+      palette.white)
+    pset(mouse_x, mouse_y, 0)
+  end
 end
